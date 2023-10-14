120a9e29e0f45e090adf4a1f4e561efea23d013c
charantejk <charantejk04@gmail.com>
2023-10-14 19:04:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Required from "./components/Required";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br />
+      <h3>React JS Task 5</h3>
+      <br />
+      <br />
+      <Required></Required>
     </div>
   );
 }
 
-export default App;
+export default App;
